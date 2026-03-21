6f016ef68b1e0d648e94c5b8b717093f5fcce658
Teston <gaspard.teston@gmail.com>
2026-03-21 11:49:28 +0100

    feat: Ajout de la page pour voir les détails des messages

--- enron_discovery/investigation/urls.py
+++ enron_discovery/investigation/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import message_list
+from .views import message_list, message_detail
 
 urlpatterns = [
     path("", message_list, name="message_list"),
+    path("<int:message_id>/", message_detail, name="message_detail"),
 ]
--- enron_discovery/investigation/views.py
+++ enron_discovery/investigation/views.py
@@ -1,14 +1,12 @@
-from django.http import HttpResponse
-
-
-def index(_request):
-    return HttpResponse('Investigation app ready')
-
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Message
 
 
 def message_list(request):
     messages = Message.objects.all().order_by("-id")[:100]
     return render(request, "investigation/message_list.html", {"messages": messages})
+
+
+def message_detail(request, message_id):
+    message = get_object_or_404(Message, id=message_id)
+    return render(request, "investigation/message_detail.html", {"message": message})
